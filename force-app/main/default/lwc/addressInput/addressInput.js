3708027c4105122c554f79fe49c3d917b65f031d
Balaji Bondar <balajibondar@gmail.com>
2022-06-09 14:40:44 +0530

    Fix-0234456 class logic updated(09062022)

--- force-app/main/default/lwc/addressInput/addressInput.js
+++ force-app/main/default/lwc/addressInput/addressInput.js
@@ -1,6 +1,5 @@
 /* eslint-disable @lwc/lwc/no-async-operation */
 /* eslint-disable no-console */
-//updatd test LWC component
 import { LightningElement, track, api } from "lwc";
 
 export default class AddressInput extends LightningElement {
